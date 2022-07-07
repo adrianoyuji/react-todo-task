ac75f2d8489654e4ac96038b91c35e3174916ea6
Adriano Yuji <adrianoyuji@gmail.com>
2022-07-07 13:56:32 -0400

    adds not found page content

--- src/features/notFound/NotFoundPage.tsx
+++ src/features/notFound/NotFoundPage.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./styles.scss";
 const NotFoundPage = () => {
-  return <section className="container">404 not found</section>;
+  return (
+    <section className="notfound__container container">
+      <h2 className="notfound__title">Oops! This page does not exist.</h2>
+      <nav className="notfound__nav">
+        <p>Please check our other pages</p>
+        <ul className="notfound__ul">
+          <li>
+            <Link to="/" className="notfound__link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="notfound__link">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 };
 
-export default NotFoundPage;
+export default React.memo(NotFoundPage);
