747ecb0a9075707d56acf41601bb8d6231f371af
Adriano Yuji <adrianoyuji@gmail.com>
2022-07-07 21:10:47 -0400

    created ChoreDetails componetn

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NotFoundPage from "features/notFound/NotFoundPage";
 import ChorePage from "features/chores/ChorePage";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "features/home/HomePage";
+import ChoreDetails from "features/chores/ChoreDetails";
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/chores" element={<ChorePage />} />
+        <Route path="/chores">
+          <Route index element={<ChorePage />} />
+          <Route path=":choreId/*" element={<ChoreDetails />} />
+        </Route>
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
---
+++ src/features/chores/ChoreDetails.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChoreDetails = () => {
+  return <section className="container">XD</section>;
+};
+
+export default ChoreDetails;
