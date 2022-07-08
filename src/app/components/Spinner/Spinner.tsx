9ba1da309fb534884facbc2060e048a2045e9710
Adriano Yuji <adrianoyuji@gmail.com>
2022-07-07 23:10:46 -0400

    added spinner component

---
+++ src/app/components/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+const Spinner = () => {
+  return (
+    <div className="spinner__container">
+      <span className="spinner" title="Loading, please wait..." />
+    </div>
+  );
+};
+
+export default Spinner;
